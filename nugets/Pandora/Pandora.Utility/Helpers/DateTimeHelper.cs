0d7279edfa2194dc230e58e2041c6d5536646734
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-02-14 14:47:04 -0500

    Added DateTimeHelper

---
+++ nugets/Pandora/Pandora.Utility/Helpers/DateTimeHelper.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Pandora.Utility.Helpers
+{
+    internal sealed class DateTimeHelper : IDateTimeHelper
+    {
+        public DateTime GetNow() => DateTime.Now;
+        public DateTime GetUtcNow() => DateTime.UtcNow;
+    }
+}
