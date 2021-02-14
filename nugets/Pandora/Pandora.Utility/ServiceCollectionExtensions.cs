6e76c042ce9e01fa22bda8ed5a18e524e562c588
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-02-14 14:53:25 -0500

    Added dependency injection

---
+++ nugets/Pandora/Pandora.Utility/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using Pandora.Utility.Helpers;
+
+namespace Pandora.Utility
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHelpers(this IServiceCollection services)
+        {
+            return services
+                .AddTransient<IDateTimeHelper, DateTimeHelper>();
+        }
+    }
+}
