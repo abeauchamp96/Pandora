c0477e2c85afc605604eb6768123f255c712094e
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-28 15:06:31 -0400

    Added ServiceProvider mock builder

---
+++ nugets/Pandora/Pandora.TestKit/ServiceProviderMockBuilder.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+
+namespace Pandora.TestKit
+{
+    public class ServiceProviderMockBuilder
+    {
+        private readonly Mock<IServiceProvider> serviceProviderMock = new();
+
+        public ServiceProviderMockBuilder AddService<TService>(TService service)
+        {
+            this.serviceProviderMock.Setup(s => s.GetService(typeof(TService))).Returns(service);
+            return this;
+        }
+
+        public Mock<IServiceProvider> BuildServiceProvider() => this.serviceProviderMock;
+
+        public Mock<IServiceScopeFactory> BuildServiceScopeFactory()
+        {
+            Mock<IServiceScope> serviceScopeMock = new();
+            serviceScopeMock.SetupGet(s => s.ServiceProvider).Returns(this.serviceProviderMock.Object);
+
+            Mock<IServiceScopeFactory> serviceScopeFactoryMock = new();
+            serviceScopeFactoryMock.Setup(s => s.CreateScope()).Returns(serviceScopeMock.Object);
+
+            return serviceScopeFactoryMock;
+        }
+    }
+}
