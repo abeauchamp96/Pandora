f5b78ed0484d2e8a4f5b03edf6485e99167ea04c
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-28 14:41:52 -0400

    Added LoggerMock extensions

---
+++ nugets/Pandora/Pandora.TestKit/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Useless warning to check on logging", Scope = "member", Target = "~M:Pandora.TestKit.LoggerMockExtensions.State(System.Object,System.Type,System.String)~System.Boolean")]
---
+++ nugets/Pandora/Pandora.TestKit/LoggerMockExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+
+namespace Pandora.TestKit
+{
+    public static class LoggerMockExtensions
+    {
+        public static void Verify<T>(this Mock<ILogger<T>> logger, string expectedMessage, LogLevel expectedLogLevel = LogLevel.Debug, Times? times = null)
+        {
+            times ??= Times.Once();
+
+            logger.Verify(l => l.Log(
+                expectedLogLevel,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => State(v, t, expectedMessage)),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)),
+                times.Value);
+        }
+
+        private static bool State(object v, Type t, string expectedMessage)
+            => (v.ToString() ?? string.Empty) == expectedMessage;
+    }
+}
