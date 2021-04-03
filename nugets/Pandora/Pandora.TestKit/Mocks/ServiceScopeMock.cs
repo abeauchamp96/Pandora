1487856dbd38c36c16f4e621755855e89638985b
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-03 11:03:54 -0400

    Added Abstract mock for service scope

---
+++ nugets/Pandora/Pandora.TestKit/Mocks/ServiceScopeMock.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Pandora.TestKit.Mocks
+{
+
+    public sealed class ServiceScopeMock : AbstractMock<IServiceScope>
+    {
+        public void GivenAServiceProvider(IServiceProvider serviceProvider)
+            => this.Mock.SetupGet(s => s.ServiceProvider).Returns(serviceProvider);
+    }
+}
