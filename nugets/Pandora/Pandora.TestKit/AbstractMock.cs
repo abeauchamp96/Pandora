ae7cf5d0827c08001699bfd19aa9cf2b2bf483b4
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-03 10:37:54 -0400

    Added AbstractMock

---
+++ nugets/Pandora/Pandora.TestKit/AbstractMock.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Moq;
+
+namespace Pandora.TestKit
+{
+    public abstract class AbstractMock<T> where T : class
+    {
+        public T Instance => this.Mock.Object;
+        protected Mock<T> Mock { get; } = new Mock<T>();
+
+        public void ResetCallCount()
+            => this.Mock.Invocations.Clear();
+    }
+}
