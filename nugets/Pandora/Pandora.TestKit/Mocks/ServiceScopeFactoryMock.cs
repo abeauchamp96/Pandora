017e819d34164f056815fa6fe6366c1218254106
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-03 11:04:00 -0400

    Added Abstract mock for service scope factory

---
+++ nugets/Pandora/Pandora.TestKit/Mocks/ServiceScopeFactoryMock.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Pandora.TestKit.Mocks
+{
+
+    public sealed class ServiceScopeFactoryMock : AbstractMock<IServiceScopeFactory>
+    {
+        public void GivenAServiceScope(IServiceScope serviceScope)
+            => this.Mock.Setup(m => m.CreateScope()).Returns(serviceScope);
+    }
+}
