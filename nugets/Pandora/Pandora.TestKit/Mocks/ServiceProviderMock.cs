e4ea90d3aa1cd31bd30f679db48e4d9693f30ee9
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-03 11:03:43 -0400

    Added Abstract mock for service provider

---
+++ nugets/Pandora/Pandora.TestKit/Mocks/ServiceProviderMock.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Pandora.TestKit.Mocks
+{
+    public sealed class ServiceProviderMock : AbstractMock<IServiceProvider>
+    {
+        public void GivenAService<TService>(TService service)
+            => this.Mock.Setup(s => s.GetService(typeof(TService))).Returns(service);
+    }
+}
