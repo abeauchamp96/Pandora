a4ef69b9c9140c1c0e199519ad19270f0e910a53
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-03 12:59:17 -0400

    Added BaseRepository

---
+++ nugets/Pandora/Pandora.Data/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Data;
+
+namespace Pandora.Data
+{
+    public abstract class BaseRepository
+    {
+        private readonly IDataSource dataSource;
+
+        protected BaseRepository(IDataSource dataSource)
+            => this.dataSource = dataSource;
+
+        protected IDbConnection Connection => this.dataSource.Connection;
+        protected IDbTransaction? Transaction => this.dataSource.Transaction;
+    }
+}
