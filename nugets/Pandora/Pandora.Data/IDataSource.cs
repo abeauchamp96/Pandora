154d3dd309f4e6cee93e0e1ef8047f7425199ad3
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-03 12:47:44 -0400

    Added IDataSource and ITransaction

---
+++ nugets/Pandora/Pandora.Data/IDataSource.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Data;
+
+namespace Pandora.Data
+{
+    public interface IDataSource
+    {
+        IDbConnection Connection { get; }
+        IDbTransaction? Transaction { get; }
+    }
+}
---
+++ nugets/Pandora/Pandora.Data/ITransaction.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Pandora.Data
+{
+    public interface ITransaction
+    {
+        void Begin();
+        void Commit();
+        void Rollback();
+    }
+}
