bd1ed1505e3346a59219681d32ca0f6da518e6e7
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-09 17:00:47 -0400

    Added uptime model

---
+++ nugets/Pandora/Pandora.Utility/Uptimes/IUptime.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Pandora.Utility.Uptimes
+{
+    public interface IUptime<T> where T : class
+    {
+        DateTimeOffset ElapsedDateTime { get; }
+    }
+}
