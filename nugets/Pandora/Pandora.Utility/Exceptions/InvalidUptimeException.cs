f5c0c05b28c88f30fac200b0b9717401371ca121
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-10 11:41:00 -0400

    Added InvalidUptimeException

---
+++ nugets/Pandora/Pandora.Utility/Exceptions/InvalidUptimeException.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Pandora.Utility.Exceptions
+{
+    public class InvalidUptimeException : Exception
+    {
+        public InvalidUptimeException()
+        {
+        }
+
+        public InvalidUptimeException(string message) : base(message)
+        {
+        }
+    }
+}
