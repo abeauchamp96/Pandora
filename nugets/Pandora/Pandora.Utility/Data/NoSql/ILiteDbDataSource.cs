49510bc6e83709d48e07b15267a21e7596ae785f
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-04-25 13:18:24 -0400

    Added LiteDB datasource and base repository

---
+++ nugets/Pandora/Pandora.Utility/Data/NoSql/ILiteDbDataSource.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using LiteDB;
+
+namespace Pandora.Utility.Data.NoSql
+{
+    public interface ILiteDbDataSource
+    {
+        ILiteDatabase Database { get; }
+    }
+}
