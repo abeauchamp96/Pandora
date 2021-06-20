fb7ba6a8c8e2c8f5282256bd7e83ad72f176582f
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-06-20 00:25:53 -0400

    Added HealthReportStatus resource and his mapper

---
+++ nugets/Pandora/Pandora.Utility/Health/Resources/HealthReportStatus.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Pandora.Utility.Health.Resources
+{
+    public enum HealthReportStatus
+    {
+        Unhealthy,
+        Degraded,
+        Healthy,
+    }
+}
